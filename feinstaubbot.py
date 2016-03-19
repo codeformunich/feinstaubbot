7ceab24275a798db6db691b9a2c36eb47d519df1
Birgit Fullerton <birgit.fullerton@gmail.com>
2016-03-19 21:05:39 +0100

    added time and data to twitter message

--- feinstaubbot.py
+++ feinstaubbot.py
@@ -74,10 +74,10 @@ def getLatestData( csvData ):
     
 def twitterValue( name, value ):
     "Twitters value"   
-
+    now = DT.datetime.now()
     twitter = Twython(config.APP_KEY, config.APP_SECRET, config.OAUTH_TOKEN, config.OAUTH_TOKEN_SECRET)
 
-    twitter.update_status(status="Feinstaubalarm in München! Der PM10 Wert bei Messstation " + name + " liegt bei: " + value + "ug/m^3")
+    twitter.update_status(status= now.strftime("%Y-%m-%d %H:%M") + " - Feinstaubalarm in München! Der PM10 Wert bei Messstation " + name + " liegt bei: " + value + "ug/m^3")
     
     
     return
